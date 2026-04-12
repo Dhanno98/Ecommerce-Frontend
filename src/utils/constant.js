80c79fb0952fe4348264cfead72156b62b655ba1
Dhananjay Bhagat <dhananjaybhagat123@gmail.com>
2026-04-12 14:22:54 +0530

    FE-007: Set up Banner and Homepage

---
+++ src/utils/constant.js
@@ -0,0 +1,9 @@
+import bannerImageOne from "../assets/sliders/kids.png"
+import bannerImageTwo from "../assets/sliders/sofa2.png"
+import bannerImageThree from "../assets/sliders/smartTv.png"
+
+export {
+    bannerImageOne,
+    bannerImageTwo,
+    bannerImageThree
+}
